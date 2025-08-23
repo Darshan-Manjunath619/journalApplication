ed0b34c3309c964e13b7074fbb95694d15b6e7b3
Darshan-Manjunath619 <darshan.manjunath619@gmail.com>
2025-08-23 23:50:02 +0530

    Added Comment on the health Controller

--- journalApplication/src/main/java/com/darshan/journalApplication/controller/HealthCheck.java
+++ journalApplication/src/main/java/com/darshan/journalApplication/controller/HealthCheck.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("health")
 
 public class HealthCheck {
+
+    // Get request to verify the application is running
     @GetMapping
 
     public static String health(){
