77d3708e4df1b2ce6746a48ade3fbf8c03913325
Darshan-Manjunath619 <darshan.manjunath619@gmail.com>
2025-08-24 02:05:52 +0530

    Updated the Type of the ObjectId

--- journalApplication/src/main/java/com/darshan/journalApplication/repository/JournalEntryRepository.java
+++ journalApplication/src/main/java/com/darshan/journalApplication/repository/JournalEntryRepository.java
@@ -1,11 +1,12 @@
 package com.darshan.journalApplication.repository;
 
 import com.darshan.journalApplication.entity.JournalEntry;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Component;
 
 @Component
-public interface JournalEntryRepository extends MongoRepository<JournalEntry,String> {
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
 
 
 
