476c6bd37df01364a05016f7813c784861c92cfd
Darshan-Manjunath619 <darshan.manjunath619@gmail.com>
2025-08-25 09:49:12 +0530

    Added the annotation and removed the getters and setters

--- journalApplication/src/main/java/com/darshan/journalApplication/entity/JournalEntry.java
+++ journalApplication/src/main/java/com/darshan/journalApplication/entity/JournalEntry.java
@@ -1,5 +1,6 @@
 package com.darshan.journalApplication.entity;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,8 +11,11 @@ import java.util.Date;
 
 @Document(collection = "journal_entries")
 //This annonation is used to tell springboot this is a collection that will be stored as mongodb document
+@Data
+// Just adding this will add all the gettter and setter.
 
-public class JournalEntry {
+public class
+JournalEntry {
     @Id
     private ObjectId id;
 
@@ -21,35 +25,4 @@ public class JournalEntry {
 
     private LocalDateTime date;
 
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
 }
