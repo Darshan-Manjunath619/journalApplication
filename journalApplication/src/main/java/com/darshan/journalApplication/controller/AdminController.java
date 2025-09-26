012447f70095b1c81e7b31e0dd5d41f92e6bc465
Darshan-Manjunath619 <darshan.manjunath619@gmail.com>
2025-09-26 08:42:54 +0530

    Removed the unnecessary import

--- journalApplication/src/main/java/com/darshan/journalApplication/controller/AdminController.java
+++ journalApplication/src/main/java/com/darshan/journalApplication/controller/AdminController.java
@@ -3,7 +3,6 @@ package com.darshan.journalApplication.controller;
 import com.darshan.journalApplication.entity.User;
 import com.darshan.journalApplication.service.UserEntryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
